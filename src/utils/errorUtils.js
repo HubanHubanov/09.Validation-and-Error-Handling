d41f0609ceb3b9ca4c5940aee4ee1a24a4d2db31
HubanHubanov <huban.hubanov@gmail.com>
2024-05-31 18:52:37 +0300

    Fix a bug in error handling system

--- src/utils/errorUtils.js
+++ src/utils/errorUtils.js
@@ -1,14 +1,15 @@
-const { mongoose } = require("mongoose");
+const  mongoose  = require("mongoose");
 
 exports.getErrorMessage = (err) => {
     let message = "";
+
     if(err instanceof mongoose.MongooseError) {
-        message = Object.values(err.errors).at(0).message;
+        message = Object.values(err.errors).at(0).message;  
 
     } else if(err instanceof Error) {
         message = err.message;
     }
-   return message
+   return message;
 }
 
 
